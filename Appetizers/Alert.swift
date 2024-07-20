186cb8120bb7c126c95fd1268b96727275d2c670
hellodamii <daremiarchs@gmail.com>
2024-07-20 20:07:54 +0100

    error alerts

--- Appetizers/Alert.swift
+++ Appetizers/Alert.swift
@@ -5,4 +5,28 @@
 //  Created by Busha on 19/07/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let inValidData      = AlertItem(title: Text("Server Error"),
+                                              message:Text( "The data received from the server is invalid, Please contact support."),
+                                              dismissButton: .default(Text("Ok")))
+    
+    static let inValidResponse  = AlertItem(title: Text( "Server Error"),
+                                                         message: Text("Invalid response from the server. Please try again later or contact support."), dismissButton: .default(Text("Ok")))
+    
+    
+    static let invalidURL       = AlertItem(title:Text( "Server Error"),
+                                                        message: Text("There was an issue with the server. If this persists, please contact support."), dismissButton: .default(Text("Ok")))
+    
+    static let unableToComplete = AlertItem(title:Text( "Server Error"),
+                                            message: Text("Unable to complete your request at this time. Please check your internet connection."), dismissButton: .default(Text("Ok")))
+}
+
--- Appetizers/AppetizerListViewModel.swift
+++ Appetizers/AppetizerListViewModel.swift
@@ -10,16 +10,29 @@ import SwiftUI
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
-        NetworkManager.shared.getAppetizers {
-            result in
+        NetworkManager.shared.getAppetizers {result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidData:
+                        self.alertItem = AlertContext.inValidData
+                        
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                        
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.inValidResponse
+                        
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                        
+                    }
                 }
 
             }
