1a48bbf6cb0f8283c58ed0ad3be348eabef5435a
hellodamii <daremiarchs@gmail.com>
2024-07-23 20:33:10 +0100

    Remote Image

--- Appetizers/RemoteImage.swift
+++ Appetizers/RemoteImage.swift
@@ -2,7 +2,41 @@
 //  RemoteImage.swift
 //  Appetizers
 //
-//  Created by Busha on 23/07/2024.
+//  Created by Damilare on 23/07/2024.
 //
 
-import Foundation
+import SwiftUI
+
+final class ImageLoader: ObservableObject {
+    
+    @Published var image: Image? = nil
+    
+    func load(fromURLString urlString: String) {
+        NetworkManager.shared.downloadImage(fromURLString: urlString) { uiImage in
+            guard let uiImage = uiImage else {return}
+            DispatchQueue.main.async {
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+    }
+}
+
+struct RemoteImage: View {
+    
+    var image: Image?
+    
+    var body: some View {
+        image?.resizable() ?? Image("food-placeholder")
+    }
+}
+
+struct AppetizerRemoteImage: View {
+    
+    @StateObject var imageLoader = ImageLoader()
+    let urlString: String
+    
+    var body: some View {
+        RemoteImage(image: imageLoader.image)
+            .onAppear {imageLoader.load(fromURLString: urlString)}
+    }
+}
