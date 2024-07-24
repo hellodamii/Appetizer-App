69fa7b3b2f45fdb77de67f6f366f04d966311748
hellodamii <daremiarchs@gmail.com>
2024-07-24 21:33:22 +0100

    Modal

--- Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225)
+            
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+            }
+            
+            Spacer()
+            Button {
+                print("tapped")
+            } label: {
+                Text("Button")
+            }
+            .padding(.bottom, 30)
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
     }
 }
 
 #Preview {
-    AppetizerDetailView()
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
 }
