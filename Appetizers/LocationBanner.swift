59b87f2a776fda363384be61187c7810029cf680
hellodamii <daremiarchs@gmail.com>
2024-08-13 18:42:15 +0100

    Improved

---
+++ Appetizers/LocationBanner.swift
@@ -0,0 +1,18 @@
+//
+//  LocationBanner.swift
+//  Appetizers
+//
+//  Created by Busha on 13/08/2024.
+//
+
+import SwiftUI
+
+struct LocationBanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationBanner()
+}
---
+++ Appetizers/SScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollView.swift
+//  Appetizers
+//
+//  Created by Busha on 13/08/2024.
+//
+
+import SwiftUI
+
+struct SScrollView: View {
+    var body: some View {
+        ScrollView{
+            
+        }
+    }
+}
+
+#Preview {
+    SScrollView()
+}
